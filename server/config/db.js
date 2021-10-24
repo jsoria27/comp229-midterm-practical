b74100b473b1c4bc84955b8d5d1ae1f88dc339e6
jsoria27 <jsoria29@my.centennialcollege.ca>
2021-10-24 13:12:12 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://midtermpractical:gNmckQQuihEVG3rf@cluster0.7zc8s.mongodb.net/books229?retryWrites=true&w=majority"
 };
